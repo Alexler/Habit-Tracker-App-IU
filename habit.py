ec25749a558a409df87de769d9686e3e1c8606a7
alexl <lerchalex86@googlemail.com>
2025-09-09 22:06:42 +0200

    Habit Class setup

--- habit.py
+++ habit.py
@@ -5,21 +5,32 @@
 #Scope of this part:
 #This is the Habit class that will be trackable by the user
 
-import datetime
+import datetime, time
 
 class Habit:
     def __init__(self, name: str, description: str, recurrence: str):
-        # initialization of a new habit object
-
+        # initialization of a new habit
         self.name = name
         self.description = description
         self.recurrence = recurrence
-        self.create_date = datetime.datetime.now()
+        self.create_time = datetime.datetime.now()
         self.complete_time = []
 
-    def complete_habits(self):
-        self.complete_time.append(datetime.datetime.now())
-        print("Habit "+self.name+" complete")
-        print("Complete Time = "+str(self.complete_time))
+    def start_habits(self):
+        #a habit will be setup here
+        self.create_time = datetime.datetime.now()
+        print("Habit " + self.name + " started")
+        print("Time = " + str(self.create_time))
 
-Habit("Name","Beschreibunb","Wiederholung").complete_habits()
+    def complete_habits(self):
+        #a habit will be marked as complete here
+        self.complete_time.append(datetime.datetime.now())
+        print("Habit "+self.name+" marked as complete")
+        print("Time = " + str(self.complete_time[-1]))
+
+#Tests
+#drink_water = Habit("Water","dont forget to drink","Wiederholung")
+#drink_water.start_habits()
+#time.sleep(2)
+#drink_water.complete_habits()
+
