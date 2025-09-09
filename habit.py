a79201cadeaa30b708fa20972c050a697063b01d
alexl <lerchalex86@googlemail.com>
2025-09-09 22:39:14 +0200

    Habit Class setup // streak is working

--- habit.py
+++ habit.py
@@ -17,20 +17,33 @@ class Habit:
         self.complete_time = []
 
     def start_habits(self):
-        #a habit will be setup here
+        #a habit will be setup/start here
         self.create_time = datetime.datetime.now()
         print("Habit " + self.name + " started")
-        print("Time = " + str(self.create_time))
+#        print("Time = " + str(self.create_time))
 
     def complete_habits(self):
-        #a habit will be marked as complete here
+        #a habit will be marked as completed here
         self.complete_time.append(datetime.datetime.now())
         print("Habit "+self.name+" marked as complete")
-        print("Time = " + str(self.complete_time[-1]))
+#        print("Time = " + str(self.complete_time[-1]))
+
+    def get_habit_streak_(self):
+        #calculates the streak based on the habit's recurrence
+        today = datetime.datetime.now().date()
+        habit_date = self.complete_time[0].date()
+        if habit_date == today:
+            print("streak")
+        else:
+            print("no streak")
+            print("habit date="+str(habit_date))
+            print("today="+str(today))
+
+
 
 #Tests
-#drink_water = Habit("Water","dont forget to drink","Wiederholung")
-#drink_water.start_habits()
-#time.sleep(2)
-#drink_water.complete_habits()
-
+drink_water = Habit("Water","dont forget to drink","Wiederholung")
+drink_water.start_habits()
+time.sleep(2)
+drink_water.complete_habits()
+drink_water.get_habit_streak_()
