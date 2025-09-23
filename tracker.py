8e6a28d2b9c398bbc541b0b157b187167fd33e2d
alexl <lerchalex86@googlemail.com>
2025-09-23 11:20:15 +0200

    tracker class setup

--- tracker.py
+++ tracker.py
@@ -1,15 +1,44 @@
 # App = Habit Tracker App
 # Name = Alexander Lerch
 # Matrikelnr. = IU14130774
+from enum import nonmember
 
 #Scope of this part:
 #the coordination / organisation of habits between the main and habit class will be handled here
 
+from habit import Habit
+from storage import storage_tracker
 
 class Tracker:
     def __init__(self, db_name):
-        self.db_name = db_name
-
+        self.storage = storage_tracker(db_name)
         self.habits = []
-        def habits_from_db(self):
-            self.habits
+        self.load_habits_from_db
+
+        print("DB Name: ",db_name)
+
+    def load_habits_from_db(self):
+        self.habits = self.storage.load_habit()
+
+    def save_habits_to_db(self, name, description, recurrence):
+        new_habits = Habit(name, description, recurrence)
+        self.habits.append(new_habits)
+        self.storage.save_habit(new_habits)
+        print("saved new habits: ", new_habits.name, new_habits.description, new_habits.recurrence)
+
+    def complete_habits(self, habit_name):
+
+        habits_complete = None
+
+
+
+        for n in self.habits:
+            if n.name == habit_name:
+                habits_complete = n
+                break
+
+        if habits_complete:
+            habits_complete.completed.habits()
+            self.storage.save_habit(habits_complete)
+
+
